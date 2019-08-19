8bb5dff1cfe7d3cdf45b1ebf71fa7f8c3c1f0fac
lwbao <18270895950@163.com>
2019-08-19 17:36:40 +0800

    修改点东西

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function (options) {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    wx.setStorageSync('logs', logs);
+    console.log(options)
 
     // 登录
     wx.login({
